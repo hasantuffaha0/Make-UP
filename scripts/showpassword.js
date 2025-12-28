6c0aa4942effb7568c960c75885091f3929315a5
hasantuffaha0 <hasantuffaha74@gmail.com>
2025-12-29 01:10:22 +0200

    you can show the password

---
+++ scripts/showpassword.js
@@ -0,0 +1,13 @@
+function togglePassword() {
+    const password = document.querySelector(".password");
+    const icon = document.querySelector(".toggle i");
+    if (password.type === "password") {
+        password.type = "text";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+    } else {
+        password.type = "password";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+    }
+}
